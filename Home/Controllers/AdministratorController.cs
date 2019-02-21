45bad22a6e792231c0f535a21cb0d34405312279
Never give up <949230332@qq.com>
2019-02-21 14:31:24 +0800

    12

--- Home/Controllers/AdministratorController.cs
+++ Home/Controllers/AdministratorController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Home.Helpers;
+using Newtonsoft.Json;
+using Home.Models;
 namespace Home.Controllers
 {
     public class AdministratorController : Controller
@@ -13,10 +15,21 @@ namespace Home.Controllers
         /// </summary>
         /// <returns></returns>
         // GET: Administrator
-        public ActionResult Index()
+        public ActionResult AdminLogin()
         {
-
             return View();
+        }
+        [HttpGet]
+        public void AdminLogin(string txtname, string txtpwd)
+        {
+            var post = new Administrator { AdministratorAccount = txtname, AdministratorPwd = txtpwd };
+            string result = Helpers.HttpClientHelper.SendRequest("api/Get?jsonStr=" + JsonConvert.SerializeObject(post), "get");
+            int n = Convert.ToInt32(JsonConvert.DeserializeObject<Administrator>(result));
+            if (n > 0)
+            {
+                Response.Write("<script>alert('登陆成功!')</script>");
+            }
+
         }
     }
 }
